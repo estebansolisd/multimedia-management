f8d191ca8a4540d699ccbc99b14a0fc9c417a9e6
Esteban Solis <estebansolisd@gmail.com>
2024-06-25 22:21:21 -0600

    adding some error handlers

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/authRoutes';
 import categoryRoutes from './routes/categoryRoutes';
 import themeRoutes from './routes/themeRoutes';
 import contentRoutes from './routes/contentRoutes';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 dotenv.config();
 
@@ -20,6 +21,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/themes', themeRoutes);
 app.use('/api/contents', contentRoutes);
+
+app.use(errorMiddleware);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
